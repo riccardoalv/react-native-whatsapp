6de860f7758b2a09dc9d751ed76aa2e2f28178d4
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-05-01 22:16:41 -0400

    feat: add ReadIcon

--- assets/images/whatsapp/icons/index.ts
+++ assets/images/whatsapp/icons/index.ts
@@ -1,5 +1,6 @@
 import ChatIcon from "./chat-icon";
 import CommunitiesIcon from "./communities-icon";
 import UpdatesIcon from "./updates-icon";
+import ReadIcon from "./read";
 
-export { ChatIcon, CommunitiesIcon, UpdatesIcon }
+export { ChatIcon, CommunitiesIcon, UpdatesIcon, ReadIcon }
