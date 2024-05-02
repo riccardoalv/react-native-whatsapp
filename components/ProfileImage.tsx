eacd41b9a709ac074e53eac9186dbfa017142ae6
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-05-01 22:18:06 -0400

    feat: create new component Profile Image

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,16 +1,21 @@
-import { Image, StyleSheet } from "react-native";
+import { Image, ImageSourcePropType, StyleSheet } from "react-native";
 
-export default function ProfileImage(data: any){
-  console.log(data.image)
-  return <Image source={data.image} style={styles.profileImage} />
+interface ProfileImageProps {
+  image: ImageSourcePropType;
+  size: number;
 }
 
-const styles = StyleSheet.create({
-  profileImage: {
-    width: 48,
-    height: 48,
-    borderRadius: 48 / 2,
-    marginRight: 10,
-  },
-});
+export default function ProfileImage({ image, size }: ProfileImageProps) {
+  const styles = StyleSheet.create({
+    profileImage: {
+      width: size,
+      height: size,
+      borderRadius: 48 * size / 2,
+      marginRight: 10,
+    },
+  });
+
+  return <Image source={image} style={styles.profileImage} />
+}
+
 
