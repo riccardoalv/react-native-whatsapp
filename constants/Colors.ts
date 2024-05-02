84d89671fda85a96c9fbc211f5d76ea16f95f36f
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-05-01 22:20:13 -0400

    feat: add colorscheme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,70 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
   light: {
     text: '#000',
     background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    icon: "black",
+    grey: "#7E858B",
+    lightGrey: "#586168",
+    title: "#1E2428",
+    shadowColor: "black",
+    tabBar: {
+      icon: "#14171B",
+      iconFocused: "#185F3F",
+      background: "white",
+      backgroundFocused: "#D1FDD6",
+      activeTintColor: "black",
+    },
+    header: {
+      icon: "black",
+      title: "#1DAA5C",
+    },
+    unreadTextColor: "white",
+    green: "#1DAA5C",
+    messages: {
+      system: {
+        background: "#F7F7F5",
+        textColor: "#586168",
+      },
+      fromMe: {
+        background: "#E7FFDB",
+      },
+      background: "white",
+      textColor: "black",
+      readColor: "#0995CB",
+    }
   },
   dark: {
     text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+    icon: "white",
+    background: '#0B141B',
+    grey: "#7E858B",
+    lightGrey: "#86939C",
+    title: "white",
+    shadowColor: "black",
+    tabBar: {
+      icon: "white",
+      iconFocused: "#D1FDD6",
+      background: "transparent",
+      backgroundFocused: "#103629",
+      activeTintColor: "white",
+    },
+    header: {
+      icon: "white",
+      title: "white",
+    },
+    unreadTextColor: "black",
+    green: "#1DAA5C",
+    messages: {
+      system: {
+        background: "#1C272D",
+        textColor: "#86939C",
+      },
+      fromMe: {
+        background: "#005D4B",
+      },
+      background: "#1F2C34",
+      textColor: "white",
+      readColor: "#0995CB",
+    }
   },
 };
