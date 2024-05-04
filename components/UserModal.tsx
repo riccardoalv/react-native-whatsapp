d40931e7d8bad247efa8c159c606a6fdac7f4cd0
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-05-04 14:56:46 -0400

    feat: add UserModal component

---
+++ components/UserModal.tsx
@@ -0,0 +1,76 @@
+import { Modal, TouchableOpacity, ImageBackground, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import { Text, View } from '@/components/Themed';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Dispatch, SetStateAction } from 'react';
+import { ChatIcon } from '@/assets/images/whatsapp/icons';
+
+interface UserModalProps {
+  user: any
+  modalVisible: boolean
+  setModalVisible: Dispatch<SetStateAction<boolean>>
+}
+
+
+export default function UserModal({ user, modalVisible, setModalVisible }: UserModalProps) {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        setModalVisible(!modalVisible);
+      }}>
+      <TouchableOpacity
+        style={styles.modalContainer}
+        activeOpacity={1}
+        onPressOut={() => { setModalVisible(false) }}
+      >
+        <TouchableWithoutFeedback>
+          <View>
+            <TouchableOpacity>
+              <ImageBackground style={{
+                width: 280,
+                height: 280,
+                elevation: 20,
+              }}
+                source={user.profileImage}>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    paddingVertical: 5,
+                    paddingLeft: 10,
+                    backgroundColor: "rgba(0, 0, 0, 0.25)",
+                  }}
+                >
+                  {user.name}</Text>
+              </ImageBackground>
+            </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                padding: 10,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity><ChatIcon width={28} color="green" /></TouchableOpacity>
+              <TouchableOpacity><MaterialIcons name="call" size={28} color="green" /></TouchableOpacity>
+              <TouchableOpacity><MaterialCommunityIcons name="video-outline" size={28} color="green" /></TouchableOpacity>
+              <TouchableOpacity><MaterialIcons name="info-outline" size={28} color="green" /></TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </TouchableOpacity>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    paddingBottom: 270
+  },
+});
