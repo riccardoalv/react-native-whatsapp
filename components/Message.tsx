1ce3c62ef15d0c4bacc55daa2e354cf219f23557
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-05-04 22:01:13 -0400

    feat: add image support

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from '@/components/Themed';
 import { ReadIcon } from '@/assets/images/whatsapp/icons';
-import { StyleSheet, useColorScheme } from 'react-native';
+import { Image, StyleSheet, useColorScheme } from 'react-native';
 import Colors from '@/constants/Colors';
 
 interface MessageProps {
@@ -11,6 +11,11 @@ export default function Message({ message }: MessageProps) {
 
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme ?? "dark"];
+
+  let sizes = {}
+
+  message.image ? sizes = Image.resolveAssetSource(message.image) : ""
+  console.log(sizes.width)
 
   return (
     <View
@@ -28,22 +33,48 @@ export default function Message({ message }: MessageProps) {
           backgroundColor: message.system ? theme.messages.system.background : message.fromMe ? theme.messages.fromMe.background : theme.messages.background,
         }, styles.message]}
       >
-        <Text style={[{
-          color: message.system ? theme.messages.system.textColor : theme.messages.textColor,
-          fontSize: message.system ? 14 : 16,
-          maxWidth: message.system ? "80%" : "60%",
-          textAlign: message.system ? "center" : "left",
-        }, styles.text]}>{message.text}</Text>
-        {message.system ? "" : (
-          <View style={[{
-            backgroundColor: message.system ? theme.messages.system.background : message.fromMe ? theme.messages.fromMe.background : theme.messages.background,
-          }, styles.hourAndStatus]}>
+        {message.image ? (
+          <>
+            <Image
+              style={{
+                maxWidth: 256,
+                maxHeight: 256,
+                borderRadius: 12,
+              }}
+              source={message.image}
+            />
+            <View style={[{
+              backgroundColor: "transparent",
+              position: "absolute",
+              justifyContent: "flex-end",
+            }, styles.hourAndStatus]}>
+              <Text style={{
+                fontSize: 13,
+                fontWeight: "500",
+                color: "white",
+                marginLeft: Math.min(sizes.width, 256) - 35,
+                marginBottom: 10,
+              }}>{message.hour}</Text>
+              {message.fromMe ? (<ReadIcon color={theme.messages.readColor} />) : ""}
+            </View>
+          </>
+        ) : (
+          <Text style={[{
+            color: message.system ? theme.messages.system.textColor : theme.messages.textColor,
+            fontSize: message.system ? 14 : 16,
+            maxWidth: message.system ? "80%" : "60%",
+            textAlign: message.system ? "center" : "left",
+          }, styles.text]}>{message.text}
+          </Text>
+        )}
+        {message.system || message.image ? "" : (
+          <View style={styles.hourAndStatus}>
             <Text style={{ fontSize: 12, color: theme.lightGrey }}>{message.hour}</Text>
             {message.fromMe ? (<ReadIcon color={theme.messages.readColor} />) : ""}
           </View>
         )}
       </View>
-    </View>
+    </View >
   )
 }
 
@@ -51,8 +82,8 @@ const styles = StyleSheet.create({
   message: {
     flexDirection: "row",
     alignItems: "flex-end",
-    paddingHorizontal: 10,
     paddingVertical: 6,
+    paddingHorizontal: 10,
     elevation: 1,
   },
   text: {
@@ -65,5 +96,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "flex-end",
     alignItems: "center",
+    backgroundColor: "transparent",
   }
 });
